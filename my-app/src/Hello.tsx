360cdf14c4a128dd9759291ab478066864878f32
athanrous <athanrous@gmail.com>
2018-06-21 17:24:04 +0200

    Improve methods and handle state better

--- my-app/src/Hello.tsx
+++ my-app/src/Hello.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import './Hello.css';
 
 export interface State {
+  displaySubject: string;
   subject: string;
 }
 
@@ -10,30 +11,31 @@ export interface Props{
 }
 
 class Greeter extends React.Component<Props, State> {
+
+  public state = {
+    displaySubject: '',
+    subject: ''
+  }
+
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
-    this.updateInputValue = this.updateInputValue.bind(this);
-    this.state = {subject: ''};
-  }
-
-  public componentDidMount() {
-    this.setState({subject: ''});
+    this.handleSubjectChange = this.handleSubjectChange.bind(this);
   }
 
   public handleClick() {
-    this.setState({subject: this.state.subject});
+    this.setState({displaySubject: this.state.subject});
   }
 
-  public updateInputValue(evt){
-    this.state = {subject: evt.target.value};
+  public handleSubjectChange(event) {
+    this.setState({subject:event.target.value});
   }
 
   public render() {
     return (
       <div>
-        <SubjectInput name={this.state.subject} onClick={this.handleClick} onChange={this.updateInputValue} />
-        <Greeting name={this.state.subject} onChange={this.updateInputValue}/>
+        <SubjectInput name={this.state.subject} onClick={this.handleClick} onChange={this.handleSubjectChange} />
+        <Greeting name={this.state.displaySubject} />
       </div>
     );
   }
@@ -43,7 +45,7 @@ function SubjectInput(props){
   return (
     <div>
       <input type="text" onChange={props.onChange}/>
-      <button onClick={props.onClick}> Click me! </button>
+      <button onClick={props.onClick}> Click Me! </button>
     </div>
   );
 }
