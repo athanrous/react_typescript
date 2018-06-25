c6ce14f552597d18747fdad323a1a834faee9865
Athanasios-Ilias Rousinopoulos <athanrous@gmail.com>
2018-06-25 21:40:21 +0200

    Add new interface

--- my-app/src/Hello.tsx
+++ my-app/src/Hello.tsx
@@ -1,9 +1,26 @@
+import axios from 'axios';
 import * as React from 'react';
 import './Hello.css';
+
+export interface GitHub {
+  assignee: string,
+  author_association: string,
+  html_url: string,
+  id: number,
+  number: number,
+  state: string,
+  title: string,
+  user: string,
+  user_html: string,
+  created_at: string,
+  updated_at: string,
+  label: string
+}
 
 export interface State {
   displaySubject: string;
   subject: string;
+  issueData: Array<GitHub>;
 }
 
 export interface Props{
@@ -14,7 +31,8 @@ class Greeter extends React.Component<Props, State> {
 
   public state = {
     displaySubject: '',
-    subject: ''
+    githubData: [],
+    subject: '',
   }
 
   constructor(props) {
@@ -22,9 +40,11 @@ class Greeter extends React.Component<Props, State> {
     this.handleClick = this.handleClick.bind(this);
     this.handleSubjectChange = this.handleSubjectChange.bind(this);
   }
-
+   
   public handleClick() {
     this.setState({displaySubject: this.state.subject});
+    axios.get('https://api.github.com/repos/Microsoft/TypeScript/pulls?state=open')
+    .then(response => this.setState({issueData: response.data}))
   }
 
   public handleSubjectChange(event) {
